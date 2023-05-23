ae26e379e8819385318e2197e6ea9e4965d10ce3
elias.merati <elias.merati@gmail.com>
2023-05-23 15:18:42 +0430

    Input Template And Optimize It

---
+++ MyEMShop.Data/Dtos/UserDto/UserAccountDto.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyEMShop.Data.Dtos.UserDto
+{
+    public class RegisterDto
+    {
+        [Display(Name = "نام کاربری")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(150)]
+        public string UserName { get; set; }
+
+        [Display(Name = "ایمیل")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(150)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Display(Name = "کلمه عبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = " تکرار کلمه عبور ")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200)]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage = "کلمه های عبور مغایرت دارند")]
+        public string RePassword { get; set; }
+    }
+}
---
+++ MyEMShop.EndPoint/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace MyEMShop.EndPoint.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Route("Register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
