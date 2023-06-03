e8409f763494ec7619b6d658698cb82a9f79e5f6
elias.merati <elias.merati@gmail.com>
2023-06-03 15:41:05 +0430

    User Management With Admin

---
+++ MyEMShop.Application/Interfaces/IManageUserService.cs
@@ -0,0 +1,14 @@
+﻿using MyEMShop.Data.Entities.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEMShop.Application.Interfaces
+{
+    public interface IManageUserService
+    {
+        IList<User> GetUsers(int pageId=1 , string FilterEmail = "" , string FilterUserName = "");
+    }
+}
---
+++ MyEMShop.Application/Services/ManageUserService.cs
@@ -0,0 +1,29 @@
+﻿using MyEMShop.Application.Interfaces;
+using MyEMShop.Data.Context;
+using MyEMShop.Data.Entities.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEMShop.Application.Services
+{
+    public class ManageUserService : IManageUserService
+    {
+        #region Injection Context
+        private readonly DatabaseContext _db;
+        public ManageUserService(DatabaseContext db)
+        {
+            _db = db;
+        }
+
+        #endregion
+
+
+        public IList<User> GetUsers(int pageId = 1, string FilterEmail = "", string FilterUserName = "")
+        {
+            IQueryable<User> Result = _db.Users;
+        }
+    }
+}
