903f5f63302d9ade89567b43fbf037e3d05667bb
elias.merati <elias.merati@gmail.com>
2023-06-24 12:56:22 +0430

    Update Solution

--- MyEMShop.Common/ShamsiDate.cs
+++ MyEMShop.Common/ShamsiDate.cs
@@ -9,8 +9,8 @@ namespace MyEMShop.Common
         {
             PersianCalendar pc = new PersianCalendar();
             return pc.GetYear(value) + "/" 
-                + pc.GetMonth(value).ToString("00") + "/" 
-                + pc.GetDayOfMonth(value).ToString("00");
+                 + pc.GetMonth(value).ToString("00") + "/" 
+                 + pc.GetDayOfMonth(value).ToString("00");
         }
     }
 }
