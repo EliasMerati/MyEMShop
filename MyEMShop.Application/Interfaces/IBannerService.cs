cb473c509b10a5007d8b3cd8617f36d046cacc05
elias.merati <elias.merati@gmail.com>
2023-08-15 12:29:33 +0330

    Add Banner To entities And Make Interface For it

---
+++ MyEMShop.Application/Interfaces/IBannerService.cs
@@ -0,0 +1,22 @@
+﻿using MyEMShop.Data.Entities.Banners;
+using System.Collections.Generic;
+
+namespace MyEMShop.Application.Interfaces
+{
+    public interface IBannerService
+    {
+        List<Banner> GetAllBanners();
+        void AddRightBanner(Banner banner);
+        void RemoveRightBanner(Banner banner);
+        void AddLeftBanner(Banner banner);
+        void RemoveLeftBanner(Banner banner);
+        void AddMiddleLeftBanner(Banner banner);
+        void RemoveMiddleLeftBanner(Banner banner);
+        void AddMiddleRightBanner(Banner banner);
+        void RemoveMiddleRightBanner(Banner banner);
+        void AddLargeRightBanner(Banner banner);
+        void RemoveLargeRightBanner(Banner banner);
+        void AddLargeLeftBanner(Banner banner);
+        void RemoveLargeLeftBanner(Banner banner);
+    }
+}
---
+++ MyEMShop.Data/Entities/Banners/Banner.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyEMShop.Data.Entities.Banners
+{
+    public class Banner
+    {
+        [Key]
+        public int BannerId { get; set; }
+
+        [Display(Name ="نام بنر")]
+        [MaxLength(50)]
+        public string BannerName { get; set; }
+
+        [Display(Name = "عکس بنر")]
+        [MaxLength(200)]
+        public string BannerImage { get; set; }
+
+        [Display(Name = "لینک بنر")]
+        [MaxLength(100)] 
+        public string BannerLink { get; set; }
+    }
+}
