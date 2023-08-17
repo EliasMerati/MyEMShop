9d0c4590a03bda526e3086e74292f5c50b8d70d8
elias.merati <elias.merati@gmail.com>
2023-08-17 13:26:13 +0330

    Rename Picture For Sample Banner

--- MyEMShop.Application/Services/BigBannerService.cs
+++ MyEMShop.Application/Services/BigBannerService.cs
@@ -37,7 +37,7 @@ namespace MyEMShop.Application.Services
             }
             else
             {
-                _db.Banners.Add(new Banner { BannerImage = "sample-banner-5-600X250.jpg", BannerName = "SampleBigBanner", BannerLink = "", BannerType = Data.Dtos.BannerType.BannerType.BigBanner });
+                _db.Banners.Add(new Banner { BannerImage = "samplebanner5-BigLeft.jpg", BannerName = "SampleBigBanner", BannerLink = "", BannerType = Data.Dtos.BannerType.BannerType.BigBanner });
                 _db.SaveChanges();
             }
         }
@@ -58,7 +58,7 @@ namespace MyEMShop.Application.Services
             }
             else
             {
-                _db.Banners.Add(new Banner { BannerImage = "sample-banner-5-600X250.jpg", BannerName = "SampleBigBanner", BannerLink = "", BannerType = Data.Dtos.BannerType.BannerType.BigBanner });
+                _db.Banners.Add(new Banner { BannerImage = "samplebanner5-BigRight.jpg", BannerName = "SampleBigBanner", BannerLink = "", BannerType = Data.Dtos.BannerType.BannerType.BigBanner });
                 _db.SaveChanges();
             }
         }
