f735a29d1994fb65103c18fc9e520192914998ea
elias.merati <elias.merati@gmail.com>
2023-08-27 20:53:34 +0330

    Remove ContactUs

--- MyEMShop.EndPoint/Pages/Admin/ContactUs/DeleteContactUs.cshtml.cs
+++ MyEMShop.EndPoint/Pages/Admin/ContactUs/DeleteContactUs.cshtml.cs
@@ -1,12 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyEMShop.Application.Interfaces;
+using MyEMShop.Data.Entities.ContactUs;
 
 namespace MyEMShop.EndPoint.Pages.Admin.ContactUs
 {
     public class DeleteContactUsModel : PageModel
     {
-        public void OnGet()
+        #region Inject Service
+        private readonly IContactUsConnectionService _contactUsConnection;
+        public DeleteContactUsModel(IContactUsConnectionService contactUsConnection)
         {
+            _contactUsConnection = contactUsConnection;
+        }
+
+        #endregion
+        [BindProperty]
+        public ContactUsConection contact { get; set; }
+        public void OnGet(int id)
+        {
+            contact = _contactUsConnection.GetById(id);
+        }
+
+        public IActionResult OnPost(int id)
+        {
+            _contactUsConnection.RemoveContactUsConnection(id);
+            return RedirectToPage("Index");
         }
     }
 }
