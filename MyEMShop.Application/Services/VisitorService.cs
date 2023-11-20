feca869a79baaf64a69a0bae67cfff09d012c34e
elias.merati <elias.merati@gmail.com>
2023-11-20 11:24:08 +0330

    Seo Fixes for meta description

--- MyEMShop.Application/Services/VisitorService.cs
+++ MyEMShop.Application/Services/VisitorService.cs
@@ -77,12 +77,24 @@ namespace MyEMShop.Application.Services
         }
         public long MonthVisitors()
         {
-            var start = DateTime.Now.AddMonths(-1);
-            var end = DateTime.Now;
+            var start = DateTime.Now.Date;
+            var end = GetMonthEndDate(DateTime.Now);
 
             return _db.Visitors.Where(v => v.Time >= start && v.Time < end).GroupBy(v => v.VisitID).LongCount();
         }
 
+        private  DateTime GetMonthEndDate(DateTime date)
+        {
+            DateTime endDate = date;
+            int endDateMonth = endDate.Month;
+
+            while (endDateMonth == endDate.Month)
+                endDate = endDate.AddDays(1);
+
+            endDate = endDate.AddDays(-1);
+
+            return endDate;
+        }
         public long TodayVisits()
         {
             var start = DateTime.Now.Date;
@@ -92,8 +104,8 @@ namespace MyEMShop.Application.Services
         }
         public long MonthVisits()
         {
-            var start = DateTime.Now.AddMonths(-1);
-            var end = DateTime.Now;
+            var start = DateTime.Now.Date;
+            var end = GetMonthEndDate(DateTime.Now);
 
             return _db.Visitors.Where(v => v.Time >= start && v.Time < end).LongCount();
         }
