075ad9a20b121ae1ce1e51cf788e78ccea59e454
EliAs <EliAs@DESKTOP-K95LG4P>
2023-12-14 11:34:45 +0330

    Bug fix

---
+++ MyEMShop.Data/Migrations/20231214070418_firstinit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyEMShop.Data.Migrations
+{
+    public partial class firstinit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1,
+                column: "RegisterDate",
+                value: new DateTime(2023, 12, 14, 10, 34, 16, 993, DateTimeKind.Local).AddTicks(3842));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1,
+                column: "RegisterDate",
+                value: new DateTime(2023, 10, 31, 15, 38, 32, 311, DateTimeKind.Local).AddTicks(4229));
+        }
+    }
+}
