dcf50913b662327708e082aa2c8628e28f0c538f
EliAs <EliAs@DESKTOP-K95LG4P>
2023-12-19 22:09:19 +0330

    Update

--- MyEMShop.Application/Interfaces/IOrderService.cs
+++ MyEMShop.Application/Interfaces/IOrderService.cs
@@ -1,7 +1,6 @@
 ﻿using MyEMShop.Data.Dtos.Order;
 using MyEMShop.Data.Dtos.OrderState;
 using MyEMShop.Data.Entities.Order;
-using MyEMShop.Data.Entities.Product;
 using System;
 using System.Collections.Generic;
 
@@ -10,21 +9,21 @@ namespace MyEMShop.Application.Interfaces
     public interface IOrderService
     {
         int AddOrder(string userName, int productId);
-        void UpdatePriceOrder(int orderId,int productId);
+        void UpdatePriceOrder(int orderId, int productId);
         Order GetOrderForUserPannel(string userName, int orderId);
         Order GetOrderById(int orderId);
         void UpdateOrder(Order order);
         bool FinallyOrder(string userName, int orderId);
-        Tuple<List<Order>,int> GetUserOrders(string userName, int pageid = 1);
+        Tuple<List<Order>, int> GetUserOrders(string userName, int pageid = 1);
         Order OrderNotPayment(string userName);
-        Tuple<List<OrdersDto>, int> GetOrdersForAdmin(OrderState orderState , int pageId=1);
-        void DeleteFromOrder(int orderId ,int productid);
+        Tuple<List<OrdersDto>, int> GetOrdersForAdmin(OrderState orderState, int pageId = 1);
+        void DeleteFromOrder(int orderId, int productid);
         bool IsOrderExist(int orderId);
         void DeleteOrder(int orderId);
         void Refresh(int quantity, int orderId, int productId);
         void ChangeStateToIsSend(int orderId);
         void ChangeStateToIsReady(int orderId);
         void ChangeStateToIsCancelled(int orderId);
-        Order ShowOrderForAdmin(int orderId ,int userId);
+        Order ShowOrderForAdmin(int orderId, int userId);
     }
 }
