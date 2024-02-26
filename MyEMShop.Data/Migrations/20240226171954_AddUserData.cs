ff4c0b6646bcd9a2c4b7cc16801701276b76fbe0
elias.merati <elias.merati@gmail.com>
2024-02-26 21:01:10 +0330

    BugFix

---
+++ MyEMShop.Data/Migrations/20240226171954_AddUserData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyEMShop.Data.Migrations
+{
+    public partial class AddUserData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1,
+                column: "RegisterDate",
+                value: new DateTime(2024, 2, 26, 20, 49, 53, 558, DateTimeKind.Local).AddTicks(9146));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1,
+                column: "RegisterDate",
+                value: new DateTime(2024, 2, 6, 21, 39, 39, 107, DateTimeKind.Local).AddTicks(4250));
+        }
+    }
+}
